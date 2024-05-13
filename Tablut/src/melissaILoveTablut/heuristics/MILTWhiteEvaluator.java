1e6a87166a705eb4dd691fc3f853011b7bfbad8a
jjulespop <giuly.fatti@gmail.com>
2024-05-13 17:15:22 +0200

    Fixed heuristics (for real)

--- Tablut/src/melissaILoveTablut/heuristics/MILTWhiteEvaluator.java
+++ Tablut/src/melissaILoveTablut/heuristics/MILTWhiteEvaluator.java
@@ -17,6 +17,14 @@ public class MILTWhiteEvaluator implements MILTEvaluator {
 			0, 3, 4, 2, 2, 2, 4, 3, 0,
 			0, 2, 3, -1, 0, -1, 3, 2, 0, 
 			0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	
+	private final static int B_W_DIFF_VAL = 30;
+	private final static int B_THREATENED = 5;
+	private final static int W_THREATENED = 10;
+	private final static int K_THREATENED = 30;
+	private final static int KING_POS_VAL = 5;
+	private final static int KING_MOV_VAL = 10;
+	private final static int FREE_WAYS_VAL = 20;
 
 	private double evaluateKingPos(MILTState state) {
 		int kingPos = state.getKing().nextSetBit(0);
@@ -26,18 +34,18 @@ public class MILTWhiteEvaluator implements MILTEvaluator {
 				if (state.hasKingEscapes()) {
 					val = Double.POSITIVE_INFINITY;
 				} else if (state.isKingThreatened()) {
-					val = -30;
+					val = -K_THREATENED;
 				} else {
-					val = kingPosVals[kingPos] * 5;
-					val += state.getKingMovements()*10;
+					val = kingPosVals[kingPos] * KING_POS_VAL;
+					val += state.getKingMovements() * KING_MOV_VAL;
 				}
 
 			} else {
 				if (state.isKingThreatened()) {
 					val = Double.NEGATIVE_INFINITY;
 				} else {
-					val = kingPosVals[kingPos] * 5;
-					val += state.getKingMovements()*10;
+					val = kingPosVals[kingPos] * KING_POS_VAL;
+					val += state.getKingMovements() * KING_MOV_VAL;
 				}
 			}
 		}
@@ -50,7 +58,7 @@ public class MILTWhiteEvaluator implements MILTEvaluator {
 		temp.or(state.getWhites());
 		temp.or(state.getBlacks());
 		temp.and(MILTState.whiteStarts);
-		return 20*(8-temp.cardinality());
+		return FREE_WAYS_VAL * (8-temp.cardinality());
 	}
 
 	public double evaluate(MILTState state) {
@@ -58,11 +66,11 @@ public class MILTWhiteEvaluator implements MILTEvaluator {
 		if (state.isTerminal()) {
 			val = (state.isWhiteWin()) ? Double.POSITIVE_INFINITY : Double.NEGATIVE_INFINITY;
 		} else {
-			val -= 30 * state.getBlacksWhitesDiff();
-			val += 10*state.getBlackPawnsThreatened();
+			val -= B_W_DIFF_VAL * state.getBlacksWhitesDiff();
+			val += B_THREATENED * state.getBlackPawnsThreatened();
 			val += this.evaluateKingPos(state);
 			val += this.evaluateFreeWays(state);
-			val -= 5*state.getWhitePawnsThreatened();
+			val -= W_THREATENED * state.getWhitePawnsThreatened();
 		}
 		return val;
 	}
